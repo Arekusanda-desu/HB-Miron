3b5d4b4a679a31f6b62faa624228410e4b83b9a7
Arekusanda-desu <sasza.zahorodnii@gmail.com>
2025-05-18 00:08:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function showGiftExplosion() {
+      const totalGifts = 105;
 
+      for (let i = 0; i < totalGifts; i++) {
+        setTimeout(() => createGift(), i * 100);
+      }
+
+      // Переход на новую страницу через 3 секунды
+      setTimeout(() => {
+        window.location.href = "second.html";
+      }, 1000);
+    }
+
+    function createGift() {
+      const gift = document.createElement('div');
+      gift.classList.add('gift');
+      gift.textContent = '🎁';
+
+      const size = 30 + Math.random() * 20;
+      gift.style.fontSize = size + 'px';
+
+      gift.style.left = Math.random() * 100 + 'vw';
+      gift.style.top = Math.random() * 100 + 'vh';
+
+      document.body.appendChild(gift);
+
+      setTimeout(() => {
+        gift.remove();
+      }, 2000);
+    }
